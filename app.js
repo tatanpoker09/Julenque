427c4fd2c0b3c56142752d7583c872a296785a69
Christian Eilers <christian@eilers.cl>
2020-12-27 17:49:36 -0300

    First version of the documentation

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const swaggerUI = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
